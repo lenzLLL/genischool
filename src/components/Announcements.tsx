ce5dcde2a58e4a3ce1ae86ae77b93aad706b4e6c
lenz <lenzyounda@gmail.com>
2024-11-17 15:48:02 +0100

    announcement almost finished

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,47 +1,67 @@
-const Announcements = () => {
+import { getCurrentUser } from "@/lib/functs";
+import prisma from "@/lib/prisma";
+
+const Announcements = async () => {
+  const currentUser = await getCurrentUser()
+  const roleConditions = {
+    Teacher: { lessons: { some: { teacherId: currentUser?.id } } },
+    Student: { students: { some: { id: currentUser?.id } } },
+    Parent: { students: { some: { parentId: currentUser?.id } } },
+  };
+
+  const data = await prisma.announcement.findMany({
+    take: 4,
+    orderBy: { date: "desc" },
+    where: {
+      ...(currentUser?.role !== "admin" && {
+        OR: [
+          { classId: null },
+          { class: roleConditions[currentUser?.role as keyof typeof roleConditions] || {} },
+        ],
+      }),
+    },
+  });
+
   return (
-    <div className="bg-white w-[100%] p-4 rounded-md">
-      <div className="flex items-center w-fulls justify-between">
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs text-gray-400">View All</span>
       </div>
       <div className="flex flex-col gap-4 mt-4">
-        <div className="bg-lamaSkyLight rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-              2025-01-01
-            </span>
+        {data[0] && (
+          <div className="bg-lamaSkyLight rounded-md p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="font-medium">{data[0].title}</h2>
+              <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                {new Intl.DateTimeFormat("en-GB").format(data[0].date)}
+              </span>
+            </div>
+            <p className="text-sm text-gray-400 mt-1">{data[0].description}</p>
           </div>
-          <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-            expedita. Rerum, quidem facilis?
-          </p>
-        </div>
-        <div className="bg-lamaPurpleLight rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-              2025-01-01
-            </span>
+        )}
+        {data[1] && (
+          <div className="bg-lamaPurpleLight rounded-md p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="font-medium">{data[1].title}</h2>
+              <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                {new Intl.DateTimeFormat("en-GB").format(data[1].date)}
+              </span>
+            </div>
+            <p className="text-sm text-gray-400 mt-1">{data[1].description}</p>
           </div>
-          <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-            expedita. Rerum, quidem facilis?
-          </p>
-        </div>
-        <div className="bg-lamaYellowLight rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
-            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-              2025-01-01
-            </span>
+        )}
+        {data[2] && (
+          <div className="bg-lamaYellowLight rounded-md p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="font-medium">{data[2].title}</h2>
+              <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                {new Intl.DateTimeFormat("en-GB").format(data[2].date)}
+              </span>
+            </div>
+            <p className="text-sm text-gray-400 mt-1">{data[2].description}</p>
           </div>
-          <p className="text-sm text-gray-400 mt-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-            expedita. Rerum, quidem facilis?
-          </p>
-        </div>
+        )}
       </div>
     </div>
   );
